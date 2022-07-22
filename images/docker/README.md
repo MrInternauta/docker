d65b72a531fb04981c92bc25528693282c59d2d1
Felipe Ramirez <feldjesus@gmail.com>
2022-07-22 14:43:38 -0500

    :art: Add test and jenkins file

--- images/docker/README.md
+++ images/docker/README.md
@@ -1,7 +1,10 @@
-# Curso de Docker en Platzi
+# RUN THE PROJECT
+## Build the image
+```bash
+docker build -t mrinternauta/node_app:001 -f Dockerfile .
+```
 
-Ésta es una aplicación de ejemplo para el curso de Docker de Platzi por Guido
-Vilariño.
-
-Encuentra más información en https://platzi.com, suscríbete al curso y aprende
-a usar Docker de manera profesional.
+## Run container
+```bash
+docker run -p 3000:3000 mrinternauta/node_app:001
+```
--- images/docker/index.js
+++ images/docker/index.js
@@ -2,20 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const MongoClient = require('mongodb').MongoClient
+const sum = require('./sum');
+// const MongoClient = require('mongodb').MongoClient
 
-// Connection URL
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+// // Connection URL
+// const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
-app.get('/', (req, res) => {
-  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
-    if (err) {
-      res.status(500).send('💥 POOM 💥: ' + err);
-    } else {
-      res.send('Me conecté a la DB!!! 😎');
-      db.close();
-    }
-  });
+// app.get('/', (req, res) => {
+//   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+//     if (err) {
+//       res.status(500).send('💥 POOM 💥: ' + err);
+//     } else {
+//       res.send('Me conecté a la DB!!! 😎');
+//       db.close();
+//     }
+//   });
+// });
+app.get('/add/:a/:b', (ctx, next) => {
+  const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+  return ctx.body = { result };
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
